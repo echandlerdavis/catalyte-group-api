944bd1c9aa8051701b56c51de766aacf660ef3d8
Devin Duval <dduval@catalyte.io>
2023-04-04 09:57:52 -0700

    added documentation and fixed findAllPurchases

--- src/main/java/io/catalyte/training/sportsproducts/domains/purchase/PurchaseController.java
+++ src/main/java/io/catalyte/training/sportsproducts/domains/purchase/PurchaseController.java
@@ -2,6 +2,8 @@ package io.catalyte.training.sportsproducts.domains.purchase;
 
 import static io.catalyte.training.sportsproducts.constants.Paths.PURCHASES_PATH;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +31,12 @@ public class PurchaseController {
     this.purchaseService = purchaseService;
   }
 
+  /**
+   * Handles a POST request to /purchases. This creates a new purchase that gets saved to the database.
+   *
+   * @param purchase
+   * @return
+   */
   @PostMapping
   public ResponseEntity savePurchase(@RequestBody Purchase purchase) {
 
@@ -37,8 +45,14 @@ public class PurchaseController {
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 
+  /**
+   * Handles a GET request directed at /purchases.
+   *
+   * @return ResponseEntity with a body containing an empty array and http status 200
+   */
   @GetMapping
   public ResponseEntity findAllPurchases() {
-    return new ResponseEntity<>(purchaseService.findAllPurchases(), HttpStatus.OK);
+    List<Purchase> emptyList = new ArrayList<>();
+    return new ResponseEntity<>(emptyList, HttpStatus.OK);
   }
 }
