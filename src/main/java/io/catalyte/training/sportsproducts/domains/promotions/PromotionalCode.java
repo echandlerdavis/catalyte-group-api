0bd0fbd475e06257ef0c911acf40d840fed852a3
Casey Gandy <cgandy@catalyte.io>
2023-04-05 17:09:32 -0400

    set basic paramaters for 'PromotionalCode' class

--- src/main/java/io/catalyte/training/sportsproducts/domains/promotions/PromotionalCode.java
+++ src/main/java/io/catalyte/training/sportsproducts/domains/promotions/PromotionalCode.java
@@ -1,4 +1,67 @@
 package io.catalyte.training.sportsproducts.domains.promotions;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class PromotionalCode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String description;
+    private String type;
+    private Double rate;
+
+    public PromotionalCode() {}
+
+    public PromotionalCode(String title, String description, String type, Double rate) {
+        this.title = title;
+        this.description = description;
+        this.type = type;
+        this.rate = rate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getRate() {
+        return rate;
+    }
+
+    public void setRate(Double rate) {
+        this.rate = rate;
+    }
 }
