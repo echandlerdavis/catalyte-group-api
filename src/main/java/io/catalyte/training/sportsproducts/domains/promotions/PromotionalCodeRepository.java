26150c505fb94e13ae66e1a454835b45687ea5a5
Casey Gandy <cgandy@catalyte.io>
2023-04-06 11:03:33 -0400

    add a repository interface for performing CRUD operations on the 'PromotionalCode'

--- src/main/java/io/catalyte/training/sportsproducts/domains/promotions/PromotionalCodeRepository.java
+++ src/main/java/io/catalyte/training/sportsproducts/domains/promotions/PromotionalCodeRepository.java
@@ -1,7 +1,27 @@
 package io.catalyte.training.sportsproducts.domains.promotions;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+/**
+ *  Repository interface for performing CRUD operations on PromotionalCode entity.
+ */
 
 public interface PromotionalCodeRepository extends JpaRepository<PromotionalCode, Long> {
 
+    /**
+     *  Saves the promotional code in the database
+     *
+     * @param promotionalCode the promotional code to be saved
+     * @return the saved promotional code
+     */
+    PromotionalCode save(PromotionalCode promotionalCode);
+
+    /**
+     * Finds a promotional code by its id
+     *
+     * @param id the id of the promotional code to find
+     * @return the promotional code with the given id, or null if not found
+     */
+    Optional<PromotionalCode> findById(Long id);
 }
