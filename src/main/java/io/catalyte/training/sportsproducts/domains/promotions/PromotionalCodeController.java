a29794e7df571140b7bf157e630884edf7a27e85
Casey Gandy <cgandy@catalyte.io>
2023-04-06 10:43:04 -0400

    created 'promotionalCodeController' code to identify endpoints for the 'promotions' domain; included JDocs

--- src/main/java/io/catalyte/training/sportsproducts/domains/promotions/PromotionalCodeController.java
+++ src/main/java/io/catalyte/training/sportsproducts/domains/promotions/PromotionalCodeController.java
@@ -1,4 +1,62 @@
 package io.catalyte.training.sportsproducts.domains.promotions;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
 public class PromotionalCodeController {
+
+    /**
+     *  Exposes endpoints for the 'promotions' domain.
+     */
+    @RestController
+    @RequestMapping("/promotional-codes")
+    public class PromotionalCodeController {
+
+        private final PromotionalCodeService promotionalCodeService;
+
+        /**
+         * Constructor for injecting the promotional code service dependency.
+         *
+         * @param promotionalCodeService Promotional code service.
+         */
+        public PromotionalCodeController(PromotionalCodeService promotionalCodeService) {
+            this.promotionalCodeService = promotionalCodeService;
+        }
+
+        /**
+         * Endpoint for creating a new promotional code.
+         *
+         * @param promotionalCodeDTO DTO object containing promotional code data.
+         * @return The created promotional code.
+         */
+        @PostMapping
+        @ResponseStatus(HttpStatus.CREATED)
+        public PromotionalCode createPromotionalCode(@RequestBody @Valid PromotionalCodeDTO promotionalCodeDTO) {
+            return promotionalCodeService.createPromotionalCode(promotionalCodeDTO);
+        }
+
+        /**
+         * Exception handler for validation errors in the request body.
+         *
+         * @param ex Exception containing the validation errors.
+         * @return A list of error messages.
+         */
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        @ResponseStatus(HttpStatus.BAD_REQUEST)
+        public List<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+            List<String> errors = new ArrayList<>();
+            ex.getBindingResult().getAllErrors().forEach((error) -> {
+                String errorMessage = error.getDefaultMessage();
+                errors.add(errorMessage);
+            });
+            return errors;
+        }
+
+    }
 }
