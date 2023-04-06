21815d64ba42318b792b0d90d5888a95ce93c615
Casey Gandy <cgandy@catalyte.io>
2023-04-06 11:07:33 -0400

    add JDocs to 'PromotionalCodeService'

--- src/main/java/io/catalyte/training/sportsproducts/domains/promotions/PromotionalCodeService.java
+++ src/main/java/io/catalyte/training/sportsproducts/domains/promotions/PromotionalCodeService.java
@@ -1,5 +1,9 @@
 package io.catalyte.training.sportsproducts.domains.promotions;
 
+
+/**
+ * This interface provides an abstraction layer for the Promotional Code Service
+ */
 public interface PromotionalCodeService {
 
     PromotionalCode createPromotionalCode(PromotionalCodeDto promotionalCodeDto);
