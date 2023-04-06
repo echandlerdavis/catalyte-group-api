7ad448a7daaf3d4b33bed28df8363dde358a2e0a
Casey Gandy <cgandy@catalyte.io>
2023-04-06 13:16:08 -0400

    add JDocs to PromotionalCodeDto where needed

--- src/main/java/io/catalyte/training/sportsproducts/domains/promotions/PromotionalCodeDto.java
+++ src/main/java/io/catalyte/training/sportsproducts/domains/promotions/PromotionalCodeDto.java
@@ -1,4 +1,34 @@
 package io.catalyte.training.sportsproducts.domains.promotions;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
 public class PromotionalCodeDto {
+
+    /**
+     * Title of the promotional code.
+     */
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    /**
+     * Description of the promotional code.
+     */
+    @NotBlank
+    private String description;
+
+    /**
+     * Type of the promotional code (flat or percent).
+     */
+    @NotNull
+    private PromotionalCodeType type;
+
+    /**
+     * Rate of the promotional code (either a flat dollar amount or a percentage off).
+     */
+    @NotNull
+    private BigDecimal rate;
+
+    // getters and setters
 }
