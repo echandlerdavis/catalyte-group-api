e7164e37d2a562c4d58d82c7747291acece2bce4
Casey Gandy <cgandy@catalyte.io>
2023-04-11 10:59:45 -0400

    refactored 'PromotionsAPITest' to account for 'No Tests Found' error.

--- src/test/java/io/catalyte/training/sportsproducts/domains/promotions/PromotionsAPITest.java
+++ src/test/java/io/catalyte/training/sportsproducts/domains/promotions/PromotionsAPITest.java
@@ -1,117 +1,63 @@
 package io.catalyte.training.sportsproducts.domains.promotions;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(PromotionalCodeController.class)
 public class PromotionsAPITest {
 
-    private static final String BASE_URL = "http://localhost:%d/promotions/codes";
+    @MockBean
+    private PromotionalCodeService promotionalCodeService;
 
-    private ObjectMapper mapper;
-    private String baseUrl;
-
-    @LocalServerPort
-    private int port;
+    @Autowired
+    private MockMvc mockMvc;
 
     @BeforeEach
-    public void setUp() {
-        mapper = new ObjectMapper();
-        baseUrl = String.format(BASE_URL, port);
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void testCreatePromotionalCodeWithFlatType() throws IOException {
-        Map<String, Object> promotionalCodeMap = new HashMap<>();
-        promotionalCodeMap.put("title", "SUMMER2015");
-        promotionalCodeMap.put("description", "Our summer discount for the Q3 2015 campaign");
-        promotionalCodeMap.put("type", "FLAT");
-        promotionalCodeMap.put("rate", new BigDecimal("10.00"));
+    void createPromotionalCode_ShouldReturn201Status_WhenGivenValidInput() throws Exception {
+        PromotionalCode promotionalCode = new PromotionalCode();
+        promotionalCode.setTitle("My Promo Code");
+        promotionalCode.setDescription("This is a test promo code.");
+        promotionalCode.setType(PromotionalCodeType.PERCENT);
+        promotionalCode.setRate(new BigDecimal("10"));
 
-        String promotionalCodeJson = mapper.writeValueAsString(promotionalCodeMap);
+        when(promotionalCodeService.createPromotionalCode(any())).thenReturn(promotionalCode);
 
-        HttpURLConnection connection = (HttpURLConnection) new URL(baseUrl).openConnection();
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-Type", "application/json");
-        connection.setDoOutput(true);
-        try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
-            outputStream.writeBytes(promotionalCodeJson);
-            outputStream.flush();
-        }
-
-        int responseCode = connection.getResponseCode();
-        assertEquals(201, responseCode);
-
-        StringBuilder response = new StringBuilder();
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-        }
-
-        Map<String, Object> responseMap = mapper.readValue(response.toString(), Map.class);
-
-        assertEquals(promotionalCodeMap.get("title"), responseMap.get("title"));
-        assertEquals(promotionalCodeMap.get("description"), responseMap.get("description"));
-        assertEquals(promotionalCodeMap.get("type"), responseMap.get("type"));
-        assertEquals(promotionalCodeMap.get("rate"), new BigDecimal(responseMap.get("rate").toString()));
+        mockMvc.perform(post("/promotional-codes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": \"My Promo Code\", \"description\": \"This is a test promo code.\", \"type\": \"PERCENT\", \"rate\": 10}"))
+                .andExpect(status().isCreated());
     }
 
     @Test
-    public void testCreatePromotionalCodeWithPercentType() throws IOException {
-        Map<String, Object> promotionalCodeMap = new HashMap<>();
-        promotionalCodeMap.put("title", "SPRINGSALE");
-        promotionalCodeMap.put("description", "Get 20% off on all items in the store");
-        promotionalCodeMap.put("type", "PERCENT");
-        promotionalCodeMap.put("rate", new BigDecimal("20.00"));
+    void createPromotionalCode_ShouldReturn400Status_WhenGivenInvalidInput() throws Exception {
+        when(promotionalCodeService.createPromotionalCode(any())).thenReturn(null);
 
-        String promotionalCodeJson = mapper.writeValueAsString(promotionalCodeMap);
-
-        HttpURLConnection connection = (HttpURLConnection) new URL(baseUrl).openConnection();
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-Type", "application/json");
-        connection.setDoOutput(true);
-        try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
-            outputStream.writeBytes(promotionalCodeJson);
-            outputStream.flush();
-        }
-
-        int responseCode = connection.getResponseCode();
-        assertEquals(201, responseCode);
-
-        StringBuilder response = new StringBuilder();
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-        }
-
-        Map<String, Object> responseMap = mapper.readValue(response.toString(), Map.class);
-
-        assertEquals(promotionalCodeMap.get("title"), responseMap.get("title"));
-        assertEquals(promotionalCodeMap.get("description"), responseMap.get("description"));
-        assertEquals(promotionalCodeMap.get("type"), responseMap.get("type"));
-        assertEquals(promotionalCodeMap.get("rate"), new BigDecimal(responseMap.get("rate").toString()));
+        mockMvc.perform(post("/promotional-codes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": \"\", \"description\": \"\", \"type\": null, \"rate\": null}"))
+                .andExpect(status().isBadRequest());
     }
 }
-
 
 
 
