49459bbfeb59da407c4c48e1015ac17639110375
Devin Duval <dduval@catalyte.io>
2023-05-02 07:37:18 -0700

    pushing to get help. Integration test isn't passing.

--- src/test/java/io/catalyte/training/sportsproducts/domains/user/UserApiTest.java
+++ src/test/java/io/catalyte/training/sportsproducts/domains/user/UserApiTest.java
@@ -1,6 +1,7 @@
 package io.catalyte.training.sportsproducts.domains.user;
 
 import static io.catalyte.training.sportsproducts.constants.Paths.USERS_PATH;
+import static io.catalyte.training.sportsproducts.constants.StringConstants.AUTHORIZATION_HEADER;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -12,9 +13,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.auth.oauth2.TokenResponseException;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import io.catalyte.training.sportsproducts.constants.StringConstants;
 import io.catalyte.training.sportsproducts.data.ProductFactory;
 import io.catalyte.training.sportsproducts.domains.product.Product;
 import io.catalyte.training.sportsproducts.domains.product.ProductRepository;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -83,20 +92,41 @@ public class UserApiTest {
     //remove testUser from database
     userRepository.delete(testUser);
   }
-  public void setUserData(User emptyUser, Long id, String firstName, String lastName, String email, String role, Date lastActive){
+  public void setUserData(User emptyUser, Long id, String firstName, String lastName, String email, String role, Date lastActive) {
     emptyUser.setId(id);
     emptyUser.setFirstName(firstName);
     emptyUser.setLastName(lastName);
     emptyUser.setEmail(email);
     emptyUser.setRole(role);
     emptyUser.setLastActive(lastActive);
-  }
+  };
+
+    private String getAccessToken() {
+      try {
+        GoogleTokenResponse response = new GoogleAuthorizationCodeTokenRequest(
+            new NetHttpTransport(), new GsonFactory(),
+            StringConstants.GOOGLE_CLIENT_ID, "{client_secret}",
+            "4/P7q7W91a-oMsCeLvIaQm6bTrgtp7", "https://oauth2-login-demo.appspot.com/code")
+            .execute();
+        System.out.println("Response:");
+        System.out.println(response);
+        String token = response.getIdToken();
+        if (token != null) {
+          return token;
+        };
+      } catch (IOException e) {
+        System.out.println(e.getMessage());
+        return null;
+      }
+      return null;
+    };
 
   @Test
-  @WithMockUser(username="")
   public void updateLastActiveTest() throws Exception {
+    getAccessToken();
     final String testPath = USERS_PATH + "/" + testUser.getId().toString() + "/updateLastActive";
-    mockMvc.perform(put(testPath))
+    mockMvc.perform(put(testPath, testUser.getEmail())
+            .header(AUTHORIZATION_HEADER, "Bearer " + testUser.getEmail()))
           .andExpect(status().isOk());
   }
 
